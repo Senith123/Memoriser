62a37f1ca984e19856802f7d26c3b0bf2c28885b
Senith123 <93915740+Senith123@users.noreply.github.com>
2025-12-18 21:56:32 +1100

    Memoriser.py

---
+++ Memoriser.py
@@ -0,0 +1,33 @@
+from tkinter import*
+screen = Tk()
+screen.geometry("500x500")
+screen.title("Memoriser")
+screen.config(background = 'dark olive green')
+
+def add_item():
+    item = entry.get()
+    list.insert(END, item)
+    entry.delete(0, END)
+def delete_item():
+    item = list.curselection()
+    list.delete(item)
+open = Button (screen, text = "OPEN", bg = "olive drab", fg = "dark sea green", font = ("times", 20, "bold"))
+open.place(x = 10, y = 10)
+delete = Button (screen, text = "DELETE", bg = "olive drab", fg = "dark sea green", font = ("times", 20, "bold"), command = delete_item)
+delete.place(x = 195, y = 10)
+save = Button (screen, text = "SAVE", bg = "olive drab", fg = "dark sea green", font = ("times", 20, "bold"))
+save.place(x = 395, y = 10)
+entry = Entry(bd = 5, bg = "olive drab", fg = "dark sea green", width = 20, font = ("times", 26, "bold"))
+entry.place(x = 10, y = 85)
+add = Button (screen, text = " ADD ", bg = "olive drab", fg = "dark sea green", font = ("times", 20, "bold"), command = add_item)
+add.place(x = 395, y = 85)
+frame = Frame(screen)
+scroll = Scrollbar(frame, orient = "vertical")
+scroll.pack(side = RIGHT, fill = Y)
+list = Listbox(frame, width = 76, height = 16, yscrollcommand = scroll.set, bg = "olive drab")
+for i in range(1,100):
+    list.insert(END, i)
+list.pack(side = LEFT)
+scroll.config(command = list.yview)
+frame.place( x = 10, y = 200)
+screen.mainloop()
